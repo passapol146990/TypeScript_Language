07df204818ca77d1e3bf5e87501c1f006814e516
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-07-21 10:29:08 +0700

    Initial commit

---
+++ ts-baseic/dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+let number = 5000;
+let num = 4;
+let result = "";
+switch (num) {
+    case 1:
+        1;
+        result = "ยอดคงเหลือ" + number;
+        break;
+    case 2:
+        2;
+        result = "ฝากเงิน";
+        break;
+    case 3:
+        3;
+        result = "ถอนเงิน";
+        break;
+    default:
+        result = "ไม่มีบริการหมายเลขนี่";
+}
+console.log(result);
